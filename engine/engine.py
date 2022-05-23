f9aabf5c63b3a97d8774ff27d36190fbcba4f6fd
chirag-madaan <madchirag546@gmail.com>
2022-05-24 00:23:41 +0530

    update engine

--- engine/engine.py
+++ engine/engine.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-from collections import defaultdict
-from .search_spotify import find_song
+from search_spotify import find_song
 from scipy.spatial.distance import cdist
 from joblib import load
 
@@ -28,31 +27,14 @@ data = pd.read_csv("../data/song_data.csv")
 scaled_data = scaler.transform(data[number_cols])
 
 
-def get_song_data(song, spotify_data):
-    try:
-        song_data = spotify_data[
-            (spotify_data["name"] == song["name"])
-            & (spotify_data["year"] == song["year"])
-        ].iloc[0]
-        return song_data
+def get_mean_vector(songs_data):
 
-    except IndexError:
-        print("Finding song in Spotify.")
-        return find_song(song["name"], song["year"])
+    if songs_data == []:
+        return songs_data
 
-
-def get_mean_vector(song_list, spotify_data):
     song_vectors = []
 
-    for song in song_list:
-        song_data = get_song_data(song, spotify_data)
-        if song_data is None:
-            print(
-                "Warning: {} does not exist in Spotify or in database".format(
-                    song["name"]
-                )
-            )
-            continue
+    for song_data in songs_data:
         song_vector = song_data[number_cols].values
         song_vectors.append(song_vector)
 
@@ -60,64 +42,66 @@ def get_mean_vector(song_list, spotify_data):
     return np.mean(song_matrix, axis=0)
 
 
-# this function is probably not needed.
-def flatten_dict_list(dict_list):
+def get_songs_data(song_list, spotify_data):
+    songs_data = []
 
-    flattened_dict = defaultdict()
-    for key in dict_list[0].keys():
-        flattened_dict[key] = []
+    for song_id in song_list:
+        to_append = None
 
-    for dictionary in dict_list:
-        for key, value in dictionary.items():
-            flattened_dict[key].append(value)
+        try:
+            to_append = spotify_data[spotify_data["id"] == song_id].iloc[0]
+        except IndexError:
+            to_append = find_song(song_id=song_id)
 
-    return flattened_dict
+        if to_append.empty:
+            print(
+                "Warning: {} does not exist in Spotify and in dataset".format(song_id)
+            )
+            continue
+        else:
+            songs_data.append(to_append)
+
+    return songs_data
 
 
-def recommend_songs(song_list, spotify_data, n_songs=10):
-    metadata_cols = ["name", "year", "artists", "explicit", "id", "release_date"]
-    # {'name': ['Come ...', 'Smells ...', 'Lithium', ...], 'year': [1991, 1991, 1992, ...]}
-    song_dict = flatten_dict_list(song_list)
+def recommend_songs_from_multiple(
+    song_list, spotify_data, n_songs=10, isPlaylist=False
+):
+    songs_data = get_songs_data(song_list, spotify_data)
+    song_center = get_mean_vector(songs_data)
 
-    song_center = get_mean_vector(song_list, spotify_data)
+    if song_center == []:
+        return None
 
     scaled_song_center = scaler.transform(song_center.reshape(1, -1))
     distances = cdist(scaled_song_center, scaled_data, "cosine")
-    index = list(np.argsort(distances)[:, :n_songs][0])
+    index = list(np.argsort(distances)[:, : n_songs * 2][0])
 
     rec_songs = spotify_data.iloc[index]
-    rec_songs = rec_songs[~rec_songs["name"].isin(song_dict["name"])]
 
-    return rec_songs[metadata_cols].to_dict(orient="records")
+    if isPlaylist:
+        rec_songs = rec_songs[~rec_songs["id"].isin(song_list)]
 
+    rec_songs = rec_songs[:n_songs]
 
-playlist = [{"name": "Shape of You", "year": 2017}]
-# playlist = [
-#     {"name": "One Less Lonely Girl", "year": 2009},
-#     {"name": "Sorry", "year": 2015},
-#     {"name": "Yummy", "year": 2020},
-#     {"name": "Boyfriend", "year": 2012},
-#     {"name": "What Do You Mean?", "year": 2015},
-# ]
+    return list(rec_songs["id"])
 
 
-recommended_songs = recommend_songs(playlist, data)
+def recommend_songs_from_single(song_id, dataset, n_songs=10):
+    try:
+        song_data = dataset[dataset["id"] == song_id].iloc[0]
+    except IndexError:
+        song_data = find_song(song_id=song_id)
 
+    if song_data.empty:
+        print("Warning: {} does not exist in Spotify and in dataset.".format(song_id))
+        return None
 
-def generate_HTML(songs):
-    path = "recommendations.html"
-    with open(path, "w") as f:
-        f.write("<html><body>")
-        for song in songs:
-            f.write(
-                '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/{}" width="300" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>'.format(
-                    song["id"]
-                )
-            )
-        f.write("</html></body>")
+    song_vector = np.array(song_data[number_cols].values)
+    scaled_song_vector = scaler.transform(song_vector.reshape(1, -1))
+    distances = cdist(scaled_song_vector, scaled_data, "cosine")
+    index = list(np.argsort(distances)[:, : n_songs + 1][0])
 
-    return path
+    rec_songs = dataset.iloc[index][1:]
 
-
-print(recommended_songs)
-print("\nHTML generated at: {}".format(generate_HTML(recommended_songs)))
+    return list(rec_songs["id"])
--- engine/search_spotify.py
+++ engine/search_spotify.py
@@ -15,21 +15,20 @@ sp = spotipy.Spotify(
 )
 
 
-def find_song(name, year):
+def find_song(song_id):
     song_data = defaultdict()
-    results = sp.search(q="track: {} year: {}".format(name, year), limit=1)
-    if results["tracks"]["items"] == []:
-        return None
+    try:
+        result = sp.track(song_id)
+    except:
+        return pd.DataFrame({})
 
-    results = results["tracks"]["items"][0]
-    track_id = results["id"]
-    audio_features = sp.audio_features(track_id)[0]
+    audio_features = sp.audio_features(song_id)[0]
 
-    song_data["name"] = [name]
-    song_data["year"] = [year]
-    song_data["explicit"] = [int(results["explicit"])]
-    song_data["duration_ms"] = [results["duration_ms"]]
-    song_data["popularity"] = [results["popularity"]]
+    song_data["name"] = [result["name"]]
+    song_data["year"] = [int(result["album"]["release_date"][:4])]
+    song_data["explicit"] = [int(result["explicit"])]
+    song_data["duration_ms"] = [result["duration_ms"]]
+    song_data["popularity"] = [result["popularity"]]
 
     for key, value in audio_features.items():
         song_data[key] = value
